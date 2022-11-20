dba29c10d71c914b5db8ab8be1d41354fa05779d
Eugene_Pro <proskuryakov.e@list.ru>
2022-11-20 22:17:26 +0300

    update execution

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -8,26 +8,30 @@
 Console.Write("Введите число: ");
 
 int numIn = Convert.ToInt32(Console.ReadLine());
+int remainder = 0;
 int multiply = 10000;
 int result = 0;
 int numInInitial = numIn;
 
-for (int i = 0; i <= 4; i++)
-{
-    int remainder = numIn % 10;
-    numIn = numIn / 10;
-    result = result + remainder * multiply;
-    multiply = multiply / 10;
-}
 if (numInInitial < 10000 || numInInitial > 99999)
 {
     Console.WriteLine($"Число {numInInitial} вне диапазона");
 }
-else if (result == numInInitial && result != 0)
-{
-    Console.WriteLine($"Число {numInInitial} палиндром");
-}
 else
 {
-    Console.WriteLine($"Число {numInInitial} не является палиндромом");
+    for (int i = 0; i <= 4; i++)
+    {
+        remainder = numIn % 10;
+        numIn = numIn / 10;
+        result = result + remainder * multiply;
+        multiply = multiply / 10;
+    }
+    if (result == numInInitial && result != 0)
+    {
+        Console.WriteLine($"Число {numInInitial} палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Число {numInInitial} не является палиндромом");
+    }
 }
