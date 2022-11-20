acc23abbfa00a6f4c9d1fc29c2a662039ea63051
Eugene_Pro <proskuryakov.e@list.ru>
2022-11-20 19:39:14 +0300

    Initial homework3

---
+++ Task_19/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход
+//  пятизначное число и проверяет,
+//  является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите число: ");
+
+int numIn = Convert.ToInt32(Console.ReadLine());
+int multiply = 10000;
+int result = 0;
+int numInInitial = numIn;
+
+for (int i = 0; i <= 4; i++)
+{
+    int remainder = numIn % 10;
+    numIn = numIn / 10;
+    result = result + remainder * multiply;
+    multiply = multiply / 10;
+}
+if (numInInitial < 10000 || numInInitial > 99999)
+{
+    Console.WriteLine($"Число {numInInitial} вне диапазона");
+}
+else if (result == numInInitial && result != 0)
+{
+    Console.WriteLine($"Число {numInInitial} палиндром");
+}
+else
+{
+    Console.WriteLine($"Число {numInInitial} не является палиндромом");
+}
---
+++ Task_23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает 
+// на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число ");
+var numIn = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= numIn; i++)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+    // int numOut = i;
+    // Console.WriteLine(numOut*numOut*numOut);
+}
